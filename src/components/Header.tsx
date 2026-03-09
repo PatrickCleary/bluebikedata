25431d114d615061c6fc1df0883bc57aeb6a1bbe
patrickCleary <patrickdaviscleary@gmail.com>
2026-03-08 23:10:41 -0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export const Header: React.FC<HeaderProps> = () => {
 
   const isMobile = !useBreakpoint("md");
   return (
-    <div className="w-fit max-w-sm w-full  top-0 z-20 bg-gray-800 rounded-md px-2 md:px-4 md:py-4 py-2 text-gray-100 pointer-events-auto border border-gray-700 shadow-md">
+    <div className="w-fit md:max-w-sm w-full  top-0 z-20 bg-gray-800 rounded-md px-2 md:px-4 md:py-4 py-2 text-gray-100 pointer-events-auto border border-gray-700 shadow-md">
 
         <div className="flex flex-col gap-2 items-start">
           <h1 className="text-lg md:text-2xl truncate items-baseline font-bold font-sans">
